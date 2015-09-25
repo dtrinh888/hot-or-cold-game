57dd3ef8de929a65647639ce2ee9ac434da9478a
dtrinh888 <dtrinh888@gmail.com>
2015-09-24 18:36:15 -0700

    working on js

--- js/app.js
+++ js/app.js
@@ -12,6 +12,29 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	/*--- start new game ---*/
+  	$(".new").click(function(){
+  		$('#count').reset();
+  	});
+
+  	/*--- create random number ---*/
+  	var number = Math.floor(Math.random() * 100) + 1;
+
+  	/*--- guess button ---*/
+  	$("#guessButton").click(function(){
+  		var guess = $('#userGuess').val;
+  		if (guess == number) {
+  			$('#guessList').text("<li>You've guessed the number!!</li>");
+  		} else if (guess < number) {
+  			$('#guessList').text("<li>You'll need to go higher.</li>");
+  		} else if (guess > number) {
+  			$('#guessList').text("<li>"+"You'll need to go lower."+'</li>')
+  		}
+  	});
+
+  	/*--- guess counter ---*/
+  	counter = 0
+
 });
 
 
